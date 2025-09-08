77278533ffccf40c775e36b52129066ddf3860c3
Florian Schumann <florian.schumann@cardanofoundation.org>
2025-09-08 10:34:24 +0200

    fix(compose): fix csp for local setup

--- frontend/docker-assets/entrypoint.sh
+++ frontend/docker-assets/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-CSP_DOMAIN="*.$(echo "$VITE_API_URL" | cut -d'.' -f2-)"
+CSP_DOMAIN="$(echo "$VITE_API_URL" | cut -d'.' -f2- -s)"
+if [ -n "$CSP_DOMAIN" ] 
+then
+    CSP_DOMAIN="*.$CSP_DOMAIN"
+else
+    # if no "." in URL simply cut scheme from URL
+    CSP_DOMAIN=$(echo "$VITE_API_URL" | cut -d'/' -f3-)
+fi
 sed -i "s|__CSP_DOMAIN__|${CSP_DOMAIN}|g" /etc/nginx/conf.d/default.conf
 envsubst </app/env.global.tmp.js >/usr/share/nginx/html/env.global.js
 nginx -g 'daemon off;'
