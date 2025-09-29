02a3a31446a5a90650759c2e393171b9aa1fcd32
Satya Muntha <satya.muntha@cardanofoundation.org>
2025-09-29 23:56:26 +0100

    fix: removed enwanted vars

--- frontend/public/env.global.js
+++ frontend/public/env.global.js
@@ -4,8 +4,3 @@ window.env = {}
 
 window.env.VITE_VERSION = `0.0.0`
 window.env.VITE_API_URL = `https://api.dev.reeve.cf-deployments.org`
-
-window.env.VITE_KEYCLOAK_URL = `https://keycloak.dev.reeve.cf-deployments.org`
-window.env.VITE_KEYCLOAK_REALM = `reeve-master`
-window.env.VITE_KEYCLOAK_CLIENT_ID = `webclient`
-window.env.VITE_KEYCLOAK_REDIRECT_URL = `https://dev.reeve.cf-deployments.org/auth/login`
