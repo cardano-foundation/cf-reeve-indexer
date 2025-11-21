d566e2ce56bc0e5a262148e4febd632e4dfb7886
Marcin Czachor <mar.czachor@gmail.com>
2025-11-21 16:38:00 +0100

    chore: removed redudant files

--- frontend/src/libs/utils/toDayjs.ts
+++
@@ -1,8 +0,0 @@
-import dayjs, { Dayjs } from 'dayjs'
-
-export const toDayjs = (value: unknown): Dayjs | undefined => {
-  if (value?.$d instanceof Date) return dayjs(value.$d)
-  if (value instanceof Date) return dayjs(value)
-  if (typeof value === 'string') return dayjs(value)
-  return undefined
-}
