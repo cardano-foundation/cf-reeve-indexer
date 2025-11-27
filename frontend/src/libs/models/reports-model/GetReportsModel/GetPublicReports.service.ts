77bfcdb2f1d3dfc0d1de16721f7b992858c97300
Marcin Czachor <mar.czachor@gmail.com>
2025-11-27 10:54:48 +0100

    chore: updated public reports api models/services

--- frontend/src/libs/models/reports-model/GetReportsModel/GetPublicReports.service.ts
+++ frontend/src/libs/models/reports-model/GetReportsModel/GetPublicReports.service.ts
@@ -1,22 +1,25 @@
-import { useQuery } from '@tanstack/react-query'
+import { keepPreviousData, useQuery } from '@tanstack/react-query'
 
 import { backendReeveApi } from 'libs/api-connectors/backend-connector-reeve/api/backendReeveApi'
 import { GetPublicReportsRequest } from 'libs/api-connectors/backend-connector-reeve/api/reports/publicReportsApi.types'
 
-const getPublicReportsQuery = async (parameters: GetPublicReportsRequest) => {
+const getPublicReportsQuery = async (request: GetPublicReportsRequest) => {
   const { reportsApi } = backendReeveApi()
 
-  const data = await reportsApi.getPublicReports({ ...parameters })
+  const data = await reportsApi.getPublicReports(request)
 
   if (!data) return null
 
   return data
 }
 
-export const useGetPublicReportsModel = (parameters: GetPublicReportsRequest) => {
+export const useGetPublicReportsModel = (request: GetPublicReportsRequest, dependencies: unknown[] = [], isEnabled: boolean = true) => {
   const { data, refetch, isFetching } = useQuery({
-    queryKey: ['PUBLIC_REPORTS', parameters],
-    queryFn: () => getPublicReportsQuery({ ...parameters })
+    queryKey: ['PUBLIC_REPORTS', ...dependencies],
+    queryFn: () => getPublicReportsQuery(request),
+    enabled: isEnabled,
+    retry: false,
+    placeholderData: keepPreviousData
   })
 
   return {
