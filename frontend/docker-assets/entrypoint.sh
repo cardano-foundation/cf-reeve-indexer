3a42668f12ae9cd7ad5d1f3166097c179ff32bd0
Florian Schumann <florian.schumann@cardanofoundation.org>
2026-08-11 09:17:50 +0200

    chore(docker): use /tmp exclusively for writing files

--- frontend/docker-assets/entrypoint.sh
+++ frontend/docker-assets/entrypoint.sh
@@ -13,10 +13,16 @@ else
 fi
 
 RUNTIME_DIR=/tmp/frontend-runtime
-mkdir -p "$RUNTIME_DIR"
+NGINX_TEMP_DIR="$RUNTIME_DIR/nginx"
+mkdir -p \
+    "$NGINX_TEMP_DIR/client_body" \
+    "$NGINX_TEMP_DIR/proxy" \
+    "$NGINX_TEMP_DIR/fastcgi" \
+    "$NGINX_TEMP_DIR/uwsgi" \
+    "$NGINX_TEMP_DIR/scgi"
 sed "s|__CSP_DOMAIN__|${CSP_DOMAIN}|g" \
     /app/runtime-csp.conf.template >"$RUNTIME_DIR/csp.conf"
 envsubst '${VITE_API_URL} ${VITE_VERSION}' \
     </app/env.global.tmp.js >"$RUNTIME_DIR/env.global.js"
 
-exec nginx -g 'daemon off;'
+exec nginx -g "pid $NGINX_TEMP_DIR/nginx.pid; daemon off;"
